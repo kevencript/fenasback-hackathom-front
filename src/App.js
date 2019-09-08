46da5dc2f4e7922f5ad10daab7d4109201299e07
joselucassr <joselucasrs@gmail.com>
2019-09-08 09:04:12 -0300

    Exact path fix

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
           <Route path="/evento/villa-mix" component={VillaMix} />
           <Route path="/evento/funn-festival" component={FunnFestival} />
           <Route path="/evento/federal-music" component={FederalMusic} />
-          <Route path="/profile" component={perfil} />
+          <Route exact path="/profile" component={perfil} />
           <Route exact path="/profile/ingressos" component={listaIngressos} />
 
           <Footer />
