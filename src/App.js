2cabb658c6e7457911d04f83badf2d89025790c5
Gabriel Braga <kevencript@gmail.com>
2019-09-08 09:10:49 -0300

    ICONEEEEE

--- src/App.js
+++ src/App.js
@@ -25,7 +25,6 @@ class App extends Component {
           <Route path="/evento/federal-music" component={FederalMusic} />
           <Route exact path="/profile" component={perfil} />
           <Route exact path="/profile/ingressos" component={listaIngressos} />
-          <Route exact path="/profile/ingressos" component={listaIngressos} />
         </BrowserRouter>
       </div>
     );
