e978d874d1b03f2d3dc96c631de1cb1c4027ea51
joselucassr <joselucasrs@gmail.com>
2019-09-08 10:35:23 -0300

    Adicionei um item da asbac

--- src/pages/listaIngressos/index.js
+++ src/pages/listaIngressos/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import villa from "../../img/villa.png";
+import asbac from "../../img/asbac.png";
 import qrCodeVilla from "../../img/qr-code-villa.png";
 import { Link } from "react-router-dom";
 
@@ -45,8 +46,31 @@ export default class listaIngressos extends Component {
             </div>
           </div>
 
+          {/* <!-- Modal --> */}
+          <div class="modal fade" id="modalAsbac" tabindex="-1" role="dialog" aria-labelledby="modalAsbacTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog" role="document">
+              <div style={{ width: '95%' }} class="modal-content mx-auto">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="modalAsbacTitle">Apresente na entrada</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <img className="card-img-top" src={qrCodeVilla} alt="" />
+
+                  <p><b>Local:</b>  Centro de Entretenimento Urbano (CEU)</p>
+                  <p><b>Data:</b> 09 de maio de 2020, 08:00</p>
+                </div>
+                <div class="modal-footer">
+                  <button style={{ backgroundColor: "#35337d" }} type="button" class="btn btn-secondary" data-dismiss="modal">Blz!</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
           <div className="col-12 ">
-            <div className="row mb-5" data-toggle="modal" data-target="#modalVilla">
+            <div className="row mb-3" data-toggle="modal" data-target="#modalVilla">
               <div className="card col-12">
 
                 <div className="row p-1">
@@ -54,7 +78,23 @@ export default class listaIngressos extends Component {
                     <img className="card-img-top" src={villa} alt="" />
                   </div>
                   <div className="col-8">
-                    <b>Villa Mix 2020</b>
+                    <b>Villa Mix Brasília 2020</b>
+                    <br />
+                    Clique aqui para ver seu ingresso
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="row mb-5" data-toggle="modal" data-target="#modalAsbac">
+              <div className="card col-12">
+
+                <div className="row p-1">
+                  <div className="col-4 my-auto">
+                    <img className="card-img-top" src={asbac} alt="" />
+                  </div>
+                  <div className="col-8">
+                    <b>Entrada Asbac</b>
                     <br />
                     Clique aqui para ver seu ingresso
                   </div>
