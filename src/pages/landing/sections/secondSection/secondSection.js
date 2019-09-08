5b641825c6f80c50025e5228e584bfd149532364
joselucassr <joselucasrs@gmail.com>
2019-09-08 14:17:31 -0300

    Texto certo

--- src/pages/landing/sections/secondSection/secondSection.js
+++ src/pages/landing/sections/secondSection/secondSection.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import "./style.css";
+import './style.css';
 
-import logo from "../../../../img/logo2.png";
+import logo from '../../../../img/logo2.png';
 
 export default class SecondSection extends Component {
   render() {
@@ -12,12 +12,22 @@ export default class SecondSection extends Component {
           <div className="row">
             <div className="col-md-12">
               <img className="logotipo" alt="" src={logo}></img>
-              <h1 className="title">A final... Quem somos?</h1>
+              <h1 className="title">Venha conhecer</h1>
               <p className="sub-title">
-                Somos uma empresa junta duas áreas importantes para qualquer
-                negócio, seja ele pessoal ou empresarial. Somos artistas.
-                Desenvolvedores e Designers trabalhando para que seu projeto
-                tenha mais desempenho e beleza.
+                Inspirado nos grandes Centros de Entretenimento do mundo, O CEU
+                – Centro de Entretenimento Urbano ASBAC, traz uma proposta
+                moderna e é o único lugar onde o gosto de todos os membros da
+                família e o entretenimento onde todos podem desfrutar se cruzam.
+                É tudo o que você está procurando. É qualquer coisa, menos
+                comum. Se você ainda nunca esteve no céu, essa é sua
+                oportunidade.
+                <br />
+                <br />
+                Restaurantes, lanchonetes, grandes festas e festivais, lojas,
+                centro estético, atrações culturais: teatro, cinema ao ar livre,
+                karaokê, espetáculos, casa noturna, parque aquático,
+                churrasqueiras, espaço para prática esportiva, escolinhas de
+                esporte, eSports, air-soft, e muito mais vem por aí.
               </p>
             </div>
           </div>
